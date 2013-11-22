93cc66dd830396c827c8ee3df7dfe3216bf9c5d1
kyripapa <korina@walkinthepark.gr>
2013-11-22 18:26:34 +0200

    First demo

--- js/jquery-drawer.js
+++ js/jquery-drawer.js
@@ -4,15 +4,42 @@
 
         if (this.length > 1) {
             this.each (function () {
-                $(this).drawer();
+                $ (this).drawer ();
             });
 
             return this;
         }
 
+        this.init = function () {
+            console.log ("initialize");
+
+            $ (this).data ("width", $ (this).outerWidth ());
+
+            styles = {
+                position: "fixed",
+                top: "0px",
+                left: "0px",
+                height: "100%",
+                width: "0px",
+                display: "none"
+            };
+
+            $ (this).css (styles);
+
+
+            return this;
+
+        }
+
         this.open = function () {
 
             console.log ("open");
+
+            $ (this).css ("display", "block").animate ({
+                    width: $ (this).data ("width")
+                },
+                800
+            );
 
         }
 
@@ -20,9 +47,20 @@
 
             console.log ("close");
 
+            $ (this).animate ({
+
+                    width: "0px"
+                },
+                500,
+                function () {
+                    $ (this).css ("display", "none");
+                }
+            );
+
         }
 
-        return this;
+
+        return this.init ();
 
     };
 
